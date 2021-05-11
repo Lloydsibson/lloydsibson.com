5419c87e9fe343ef4d078a52f68816df0a60b602
Lloyd Sibson <lloydsibson@gmail.com>
2021-05-11 13:19:51 +0100

    Adds zoom icon to portfolio images and moves lightbox click to the zoom icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,23 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Card = ({ Image, CardType, CardSubTitle, Icon }) => {
   const lightboxToggle = (e) => {
     // GRAB URL FROM BACKGROUND AND MOVE IT INTO THE LIGHTBOX URL
-    const backgroundURLRAW = e.target.style.backgroundImage;
+    const backgroundURLRAW =
+      e.currentTarget.previousElementSibling.previousElementSibling.style
+        .backgroundImage;
     let backgroundURL = backgroundURLRAW.replace(`url("`, "");
     backgroundURL = backgroundURL.replace(`")`, "");
     const lightboximage = document.querySelector(".lightbox-image");
     lightboximage.setAttribute("src", backgroundURL);
 
-    // console.log(e.target.previousSibling.previousElementSibling.textContent);
+    // console.log(
+    //   e.currentTarget.previousElementSibling.previousElementSibling.style
+    //     .backgroundImage
+    // );
 
-    // if (
-    //   e.target.previousSibling.previousElementSibling.textContent === "Image"
-    // ) {
     const lightboxContainer = document.querySelector(".lightbox-container");
     lightboxContainer.classList.toggle("-open");
-    // }
   };
+
   return (
     <figure className="card-container">
       <h4 className="card-container__title">{CardType}</h4>
@@ -27,14 +29,13 @@ export const Card = ({ Image, CardType, CardSubTitle, Icon }) => {
         <div className="double-bounce2"></div>
       </div>
       <div
-        onClick={(e) => lightboxToggle(e)}
         className="card-container__image"
         style={{
           backgroundImage: `url(${Image})`,
         }}
       ></div>
       <p className="card-container__subtitle">{CardSubTitle}</p>
-      <div className="card-container__icon">
+      <div className="card-container__icon" onClick={(e) => lightboxToggle(e)}>
         <FontAwesomeIcon icon={Icon} />
       </div>
     </figure>
--- src/pages/4-PortfolioPage.jsx
+++ src/pages/4-PortfolioPage.jsx
@@ -4,7 +4,7 @@ import { Lightbox } from "../components/Lightbox";
 // import { faLink } from "@fortawesome/free-solid-svg-icons";
 // import { faVideo } from "@fortawesome/free-solid-svg-icons";
 // import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
-import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { faSearchPlus } from "@fortawesome/free-solid-svg-icons";
 
 export const PortfolioPage = () => {
   const CardData = [
@@ -13,63 +13,63 @@ export const PortfolioPage = () => {
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1620419966/img/AFTM-a4-poster.png",
       cardType: "A4 Event Poster",
       cardSubTitle: "Adobe Photoshop",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1620422039/img/eventbrite-api.png",
       cardType: "Eventbrite API Integration",
       cardSubTitle: "Figma & JavaScript ES6",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1620419966/img/apuk-nav-design.png",
       cardType: "Mobile Nav Design",
       cardSubTitle: "Figma",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://images.squarespace-cdn.com/content/v1/5db41714f991e52a4904af5f/1617378126936-NQ6HE8JEA7PVWXN31GJ4/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/unisex-basic-softstyle-t-shirt-white-front-60673b49ef12c.jpg?format=800w",
       cardType: "T-Shirt Design",
       cardSubTitle: "Adobe Photoshop & Printful",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1557013440/img/portfolio/jmgb.png",
       cardType: "JMGB Logo",
       cardSubTitle: "Adobe Photoshop",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1620419966/img/reawaken-eventbrite-banner-new-version.png",
       cardType: "Eventbrite Banner",
       cardSubTitle: "Adobe Photoshop",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://images.squarespace-cdn.com/content/v1/5db41714f991e52a4904af5f/1606669957396-6HM6WHW3K5OYB047L1QY/ke17ZwdGBToddI8pDm48kPJXHKy2-mnvrsdpGQjlhod7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmihaE5rlzFBImxTetd_yW5btdZx37rH5fuWDtePBPDaHF5LxdCVHkNEqSYPsUQCdT/mockup-62011df2.jpg?format=800w",
       cardType: "Mask Design (Covid)",
       cardSubTitle: "Adobe Photoshop & Printful",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1595526500/img/rac-logo.png",
       cardType: "RAC Logo",
       cardSubTitle: "Adobe Photoshop",
-      icon: faImage,
+      icon: faSearchPlus,
     },
     {
       image:
         "https://res.cloudinary.com/cloudinary-ls-images/image/upload/v1557013643/img/portfolio/amo-cards.png",
       cardType: "AMO ID Cards",
       cardSubTitle: "Adobe Photoshop & Printful",
-      icon: faImage,
+      icon: faSearchPlus,
     },
   ];
 
