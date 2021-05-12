90848ffd8c2d938e94c67d8615b11c0f28c3adbf
Lloyd Sibson <lloydsibson@gmail.com>
2021-05-12 17:59:44 +0100

    Adds functionality for the lightbox on the card and zoom icon

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,27 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const Card = ({ Image, CardType, CardSubTitle, Icon }) => {
   const lightboxToggle = (e) => {
     // GRAB URL FROM BACKGROUND AND MOVE IT INTO THE LIGHTBOX URL
+    const backgroundURLRAW = e.currentTarget.style.backgroundImage;
+    let backgroundURL = backgroundURLRAW.replace(`url("`, "");
+    backgroundURL = backgroundURL.replace(`")`, "");
+    const lightboximage = document.querySelector(".lightbox-image");
+    lightboximage.setAttribute("src", backgroundURL);
+
+    // ZOOM ICON
+    // e.currentTarget.previousElementSibling.previousElementSibling.style
+    // .backgroundImage;
+
+    // console.log(
+    //   e.currentTarget.previousElementSibling.previousElementSibling.style
+    //     .backgroundImage
+    // );
+
+    const lightboxContainer = document.querySelector(".lightbox-container");
+    lightboxContainer.classList.toggle("-open");
+  };
+
+  const lightboxToggleZoomIcon = (e) => {
+    // GRAB URL FROM BACKGROUND AND MOVE IT INTO THE LIGHTBOX URL
     const backgroundURLRAW =
       e.currentTarget.previousElementSibling.previousElementSibling.style
         .backgroundImage;
@@ -11,6 +32,10 @@ export const Card = ({ Image, CardType, CardSubTitle, Icon }) => {
     backgroundURL = backgroundURL.replace(`")`, "");
     const lightboximage = document.querySelector(".lightbox-image");
     lightboximage.setAttribute("src", backgroundURL);
+
+    // ZOOM ICON
+    // e.currentTarget.previousElementSibling.previousElementSibling.style
+    // .backgroundImage;
 
     // console.log(
     //   e.currentTarget.previousElementSibling.previousElementSibling.style
@@ -30,12 +55,16 @@ export const Card = ({ Image, CardType, CardSubTitle, Icon }) => {
       </div>
       <div
         className="card-container__image"
+        onClick={(e) => lightboxToggle(e)}
         style={{
           backgroundImage: `url(${Image})`,
         }}
       ></div>
       <p className="card-container__subtitle">{CardSubTitle}</p>
-      <div className="card-container__icon" onClick={(e) => lightboxToggle(e)}>
+      <div
+        className="card-container__icon"
+        onClick={(e) => lightboxToggleZoomIcon(e)}
+      >
         <FontAwesomeIcon icon={Icon} />
       </div>
     </figure>
